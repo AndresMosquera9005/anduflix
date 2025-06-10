6ed9db6a96db9d1b7b19fa7c46ec10c0874893c6
AndresMosquera9005 <andrubjj@gmail.com>
2025-06-10 16:46:45 -0500

    se hace cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,17 @@
 import { Routes } from '@angular/router';
 import { PrincipalComponent } from './components/principal/principal.component';
 import { LoginComponent } from './components/login/login.component';
+import { CrearCuentaComponent } from './components/crear-cuenta/crear-cuenta.component';
 
 export const routes: Routes = [
     {
         path: '', component: PrincipalComponent , title: 'Home page' 
     },
-    // {
-    //     path: 'home', component: PrincipalComponent , title: 'Trailer Peliculas' 
-    // },
     {
         path: 'login', component: LoginComponent , title: 'Login page' 
     },
+    {
+        path: 'crearCuenta', component: CrearCuentaComponent , title: 'Registro page'
+    }
     
 ];
